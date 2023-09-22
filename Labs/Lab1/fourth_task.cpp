2ca264996ee925322ab5444aa3b1e5b5002dbd7e
Kirill <111117870+Knhn0@users.noreply.github.com>
2023-09-22 15:24:48 +0300

    fix: complieted tasks by Olga Viktorovna

--- Labs/Lab1/fourth_task.cpp
+++ Labs/Lab1/fourth_task.cpp
@@ -1,28 +1,37 @@
 #include <bits/stdc++.h>
 
+
+// TODO: динамический массив "квадратный"
+// TODO: заполнение от -20 до 40
+// TODO: Посчитать сумму эл-тов выше гл. диагонали и на ней
+
 using namespace std;
 
 int main() {
+    int size;
+
+    cout << "Enter array size: ";
+    cin >> size;
     srand(time(NULL));
-    int arr[4][5];
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 5; j++) {
-            arr[i][j] = rand() % 100;
+    int arr[size][size];
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            arr[i][j] = rand() % 60 - 20;
         }
     }
-
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 5; j++) {
-            cout << arr[i][j] << " ";
+    cout << "\n";
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << arr[i][j] << "  ";
         }
         cout << "\n";
     }
 
     cout << "\n";
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < size; i++) {
         int sum_width = 0;
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < size; j++) {
             sum_width += arr[i][j];
         }
         cout << "Sum of " << i + 1 << " row = " << sum_width << "\n";
@@ -30,12 +39,22 @@ int main() {
 
     cout << "\n";
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < size; i++) {
         int sum_height = 0;
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < size; j++) {
             sum_height += arr[j][i];
         }
         cout << "Sum of " << i + 1 << " colomn = " << sum_height << "\n";
     }
+
+    cout << "\n";
+
+    int summa = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = i; j < size; j++) {
+            summa += arr[i][j];
+        }
+    }
+    cout << "Summa: " << summa;
     return 0;
 }
